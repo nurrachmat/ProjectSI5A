f47270d44bcb2acee32276ec611c3a8ba086b11e
ericcandra <160827840+ericcandra@users.noreply.github.com>
2024-09-10 11:16:16 +0700

    update home

--- mdpsi5a/routes/web.php
+++ mdpsi5a/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('home', function () {
+    return view('view_home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
+
+});
